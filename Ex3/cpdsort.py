bfc48d8c76e8fab77b90612eb200a5bb79549a22
Fxlipe115 <felipegraeff@hotmail.com>
2017-05-17 14:16:18 -0300

    radix done

--- Ex3/cpdsort.py
+++ Ex3/cpdsort.py
@@ -13,7 +13,6 @@ def mergeSort(l,key=None,reverse=False):
 
         return _merge(llist,rlist,key,reverse)
 
-		
 def _merge(left,right,key,reverse=False):
     f = (lambda x,y: x > y) if reverse else (lambda x,y: y > x)
     if len(left) > 0 and len(right) > 0:
@@ -21,11 +20,11 @@ def _merge(left,right,key,reverse=False):
             return [v] + _merge(left,right,key,reverse)
     else:
             return left + right
-            
-            
+
+
 def insertionSort(l):
     for i in range(1, len(l)):
-        j = i-1 
+        j = i-1
         key = l[i]
         while (l[j] > key) and (j >= 0):
            l[j+1] = l[j]
@@ -33,21 +32,31 @@ def insertionSort(l):
         l[j+1] = key
 
 
-def radixSort(l,radix=10,msd=False):
-    for i,_ in enumerate(l):
-        l[i] = int(l[i] * (10**6))
-    passes = int(round(log(max(min(l),max(l),key=abs),radix)) + 1)
-    for i in range(passes):
-        buckets = [[]]*10
-        for x in l:
-            buckets[int((x / (radix**i)) % radix)].append(x)
-        l.clear()
-        for m in buckets:
-            l.extend(m)
-			
-    
+def radixSort(li,radix=10,msd=False):
+    l2 = [[],[]]
+    for i in li:
+        if i < 0:
+            l2[0].append(i)
+        else:
+            l2[1].append(i)
+    for l in l2:
+        for i,j in enumerate(l):
+            l[i] = int(j * (10**6))
+        passes = int(round(log(max(abs(min(l)),max(l)),radix)) + 1)
+        for i in range(passes):
+            buckets = [[] for _ in range(radix)]
+            for n,x in enumerate(l):
+                buckets[x // radix**i % radix].append(x)
+            l.clear()
+            for m in buckets:
+                l.extend(m)
+        for i,j in enumerate(l):
+            l[i] = j / (10**6)
+    return l2[0] + l2[1]
+
 
 
 if __name__ == '__main__':
-    l = [7,6,5,4,3,1,6,3,4,2,7,8,0]
+    l = [7.423,6,5,4.512342,3,1,6,3,-1,4.512341,2,-2,7.217,8,0]
     print(radixSort(l))
+    print(radixSort(l,radix=2))
